2eaaab51fa36ec327b578d91e282b73b8c444ad5
algrimes <agrimes@thoughtworks.com>
2015-01-08 22:03:24 +1100

    k : wip

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,7 +3,15 @@ class DashboardController < ApplicationController
   
   def index
     require 'open-uri'
-    page = Nokogiri::HTML(open('http://www.bom.gov.au/products/IDV60901/IDV60901.95936.shtml'))
-    @view = { "temp" => page.css('td[headers=\'t1-temp\']')[0].text}
+    current_temp_page = Nokogiri::HTML(open('http://www.bom.gov.au/products/IDV60901/IDV60901.95936.shtml'))
+
+    
+    
+    forecast_page = Nokogiri::HTML(open('http://www.bom.gov.au/vic/forecasts/melbourne.shtml'))
+   
+    @view = { "current_temp" => current_temp_page.css('td[headers=\'t1-temp\']')[0].text, 
+      "tomorrow_max" => forecast_page.css('#content div:nth-of-type(2) .forecast .max').text,
+      "today_max" => forecast_page.css('#content div:nth-of-type(1) .forecast .max').text
+    }
   end
 end
